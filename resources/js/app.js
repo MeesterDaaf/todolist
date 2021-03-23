a455dae710743786cf3a8cf5ca46360fa4eda37b
meesterdaaf <d.vanoudheusden@rocva.nl>
2021-03-23 15:47:31 +0100

    back to normal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,16 @@
-// require('./bootstrap');
+require('./bootstrap');
 
-// document.addEventListener('change', (e) => {
-//     if (!e.target?.form?.matches('.todo-item__form')) {
-//         return
-//     }
+document.addEventListener('change', (e) => {
+    if (!e.target?.form?.matches('.todo-item__form')) {
+        return
+    }
 
-//     const action = e.target.form.action;
-//     const data = new FormData(e.target.form);
+    const action = e.target.form.action;
+    const data = new FormData(e.target.form);
 
-//     axios.post(action, Object.fromEntries(data.entries()))
-//         .catch(err => {
-//             console.error(err)
-//             e.target.checked = !e.target.checked
-//         })
-// })
+    axios.post(action, Object.fromEntries(data.entries()))
+        .catch(err => {
+            console.error(err)
+            e.target.checked = !e.target.checked
+        })
+})
